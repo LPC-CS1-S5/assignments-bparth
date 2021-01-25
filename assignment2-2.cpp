30181bb77b35997d6419317bdfc544d36ecb72d9
Parth <parthu@aol.in>
2021-01-25 18:19:50 +0000

    assignment2-2 added

---
+++ assignment2-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	char letter;
+
+  letter = 'A';
+  cout << letter << endl;
+  letter = 'B';
+  cout << letter << endl;
+  return 0;
+
+
+}
