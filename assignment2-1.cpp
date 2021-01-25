79e07ad649fd2bbc11a156e74a23123c18212260
Parth <parthu@aol.in>
2021-01-25 17:57:59 +0000

    version1

---
+++ assignment2-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int checking ; 
+	unsigned int miles; // unsinged : 0 ~ 2 ** 31 -1 
+	long days;
+
+	checking = -20;
+	// miles = 4276;
+	miles = 4276;
+	days = 18900;
+	cout << "We have made a long journey of " << miles << " miles.\n";
+	cout << "Our checking account balance is " << checking << "\n"; // new line
+	cout << "About " << days << " days ago Columbus stood on this spot.\n";
+	return 0;
+
+}
