022a8a6ec48c4e52951f6914624614c5fae17195
Parth <parthu@aol.in>
2021-02-23 04:20:45 +0000

    q41 and q42 QUIZ 4-1 material has been updated and commited

---
+++ assignments/q41.cpp
@@ -0,0 +1,79 @@
+//Question 1 - Geometry calculator
+
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+  //create integers and doubles so that once the user initializes them they can be used to complete operations. 
+  int choice;
+  double area, radius, length, height, pi, base, width;
+  //pi was given to us in the prompt
+  pi = 3.14159;
+
+  //giving out options to the user so that he can coose 1-4 based on what he wants to get calulated. Area of a circle, rect, or triangle are avaliable, or he can simply quit the program
+
+  cout<< "Welcome to the geometry claculator!!! Choose one option from below \n\n";
+  cout<<"1. Calculate area of a Circle: \n";
+  cout<<"2. Calculate area of a Rectangle: \n";
+  cout<<"3. Calculate area of a Triangle: \n";
+  cout<<"4. Quit \n";
+
+  // takes the user input and then wil see what the user wants to calculate
+  cin>> choice;
+
+  //makes sure that the user is giving a number under 5 to choose a proper choice
+if(choice <= 4){ 
+
+  // #1 is the area of a circle, this calculates that!
+  if(choice == 1){
+    cout<< "What's the radius?"<<endl;
+    cin>> radius;
+    if(radius >0){
+      area = pi * (radius* radius);
+      cout<<fixed<<setprecision(3)<< "the area of your circle is: " << area<<endl;
+      
+    }else{
+      cout<<"please enter a positive number";
+      return(0);
+    }
+  }
+
+  // calculates the area of the rectangle
+  if(choice == 2){
+    cout<< "What's the length and the width of the rectangle?"<<endl;
+    cin>> length>>width;
+    if(length >0 && width>0){
+      area = (length* width);
+      cout<<fixed<<setprecision(3)<< "the area of your rectangle is: " << area<<endl;
+      
+    }else{
+      cout<<"please enter a positive number";
+      return(0);
+    }
+  }
+
+  //claculates the area of a triangle
+  if(choice == 3){
+    cout<< "What's the base and the height of your triangle?"<<endl;
+    cin>> base>>height;
+    if(base >0 && height>0){
+      area = 0.5 * (base* height);
+      cout<<fixed<<setprecision(3)<< "the area of your triangle is: " << area<<endl;
+      
+    }else{
+      cout<<"please enter a positive number";
+      return(0);
+    }
+  }
+
+  //exits the program for you and tells you to have fun lol.
+  if(choice == 4){
+    cout<<"Have fun bud!"<<endl;
+    return(0);
+  }
+} else{
+  cout<<"Enter a number less than 5!!!!"<<endl;
+  return(0);
+}
+}
---
+++ assignments/q42.cpp
@@ -0,0 +1,71 @@
+// QuIESTION #2 - SHIPPING CONTAINERS
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+//creates the variables such as weight, miles, and different price rates 
+int weight, miles;
+double p1,p2,p3,p4,rate;
+
+//price rates are given to us
+p1 = 1.1;
+p2 = 2.2;
+p3 = 3.7;
+p4 = 4.8;
+
+//tells the user that we have to enter the weight and distance
+cout << "Enter the weight of your container and miles being shipped: "<<endl;
+
+//takes the wight and distance from the user.
+cin>> weight>> miles;
+
+// sure that the weight is above 0 and below 20, and that the distance is above 10 but below 3000, and also checks for no negative numbers 
+if(weight > 0 && weight <= 20 && miles >10 && miles <= 3000){
+
+  //calculates the rate for boxes under 2,6,10,20 kg and so on.
+  if(weight <= 2){
+    if(miles < 500){
+      rate = p1* miles;
+    cout<< "your rate is: "<< rate << " \n" << "Weight is: "  << weight << " \n"<< "Miles is: " << miles << endl;
+  }else{
+    //rate for the box if over 500 miles
+    rate = (miles/500)*p1;
+    cout<< "your rate is: "<< rate << " \n" << "Weight is: "  << weight << " \n"<< "Miles is: " << miles << endl;
+    }
+  }else if(weight <= 6){
+    if(miles < 500){
+      rate = p2* miles;
+    cout<< "your rate is: "<< rate << " \n" << "Weight is: "  << weight << " \n"<< "Miles is: " << miles << endl;
+  }else{
+    rate = (miles/500)*p2;
+    cout<< "your rate is: "<< rate << " \n" << "Weight is: "  << weight << " \n"<< "Miles is: " << miles << endl;
+  }
+  }else if(weight <= 10){
+    if(miles < 500){
+      rate = p3* miles;
+    cout<< "your rate is: "<< rate << " " << "Weight is: "  << weight << " "<< "Miles is: " << miles << endl;
+  }else{
+    rate = (miles/500)*p3;
+    cout<< "your rate is: "<< rate << " \n" << "Weight is: "  << weight << " \n"<< "Miles is: " << miles << endl;
+  }
+  }else {
+    if(miles < 500){
+      rate = p4*miles;
+    cout<< "your rate is: "<< rate << " \n" << "Weight is: "  << weight << " \n"<< "Miles is: " << miles << endl;
+    }else{
+      rate = (miles/500)*p4;
+    cout<< "your rate is: "<< rate << " \n" << "Weight is: "  << weight << " \n"<< "Miles is: " << miles << endl;
+    }
+
+  }
+}else{
+
+  //tells the user this warning that the number they put in was invalid. 
+  cout<< "Enter a weight less than 20 kg and greater than 0kg. And make sure your distance is under 3000 miles and over 10 miles"<<endl;
+  exit(0); 
+}
+
+}
