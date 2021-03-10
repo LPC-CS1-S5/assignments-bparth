2a30d77dc0033cee9c7b4753a238b201052a69b2
Parth <parthu@aol.in>
2021-03-10 19:00:06 +0000

    q5-2-4r commited its incomplete as i dont know how to do it properly

---
+++ assignments/q5-2-4r.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+  ///makes all the required variables for the program
+  ifstream ifs;
+  double totSal, avgSal;
+  string data;
+  //opens the required file
+  ifs.open("employee.txt");
+  if(ifs.fail()){
+    cout<<"try again later";
+    exit(0);
+  }
+
+  total = 0
+
+  for(int i = 0; i <= name; i++){
+
+  }
+}
