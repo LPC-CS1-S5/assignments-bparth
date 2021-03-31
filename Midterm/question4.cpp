ecf8280052651270ef5552b41149b4cede605e3f
Parth <parthu@aol.in>
2021-03-31 17:24:49 +0000

    q4 completed with success!

---
+++ Midterm/question4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+int getRand();
+int getRand(int);
+int getRand(int , int );
+using namespace std;
+int main(){
+  int a,b,c;
+  a = getRand();
+  b = getRand(20);
+  c = getRand(10, 20);
+
+  cout<< a <<" "<<b << " "<< c<<endl;
+}
+
+int getRand(){
+  int r=0;
+  srand((unsigned)time(0));
+  r = rand() % 100;
+}
+int getRand(int a){
+  int r=0;
+  srand((unsigned)time(0));
+  r = (rand() % a) +1;
+}
+int getRand(int a, int b){
+  int r = 0;
+  srand((unsigned)time(0));
+  r = (rand() % b) + a;
+}
