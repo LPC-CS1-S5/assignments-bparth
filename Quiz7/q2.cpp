ef8afe5cf11a677456a799f62dc72fd2cf63ba8a
Parth <>
2021-05-05 02:57:51 +0000

    q2 finished, no mistakes

--- Quiz7/q2.cpp
+++ Quiz7/q2.cpp
@@ -7,5 +7,19 @@ void printTriangle(int numbers[][3], int);
 const int N = 3;
 
 int main(){
-  int numbers[N][N];
+  int numbers[N][N] = {{0,1,2},{3,4,5},{6,7,8}};
+  printTriangle(numbers, N);
+}
+
+void printTriangle(int numbers[3][3], int N){
+  //For loop to go throught the 2d array
+
+  for(int i = 0; i<N;i++){
+    for(int j = 0; j <N; j++){
+      if(i>=j){
+        cout<<numbers[i][j]<<" ";
+      }
+    }
+    cout<<endl;
+  }
 }
