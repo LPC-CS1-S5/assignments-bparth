707bd190c1627b61720b2632d80161444f056109
Parth <>
2021-05-05 03:11:22 +0000

    partial

--- Quiz7/q3.cpp
+++ Quiz7/q3.cpp
@@ -2,4 +2,20 @@
 using namespace std;
 
 int findmax(int numbers[], int, int);
+const int N = 50;
+int main(){
+  //declared all the variables
+  int numbers[N], numbers1[20], numbers2[20], max1, max2, num, entry;
 
+  cout<< "Enter the number of items you want in your array: ";
+  cin>> num;
+
+  cout<< "Enter your numbers for the array: "<<endl;
+
+  for(int i = 0; i<num;i++){
+    cin>>numbers[entry];
+  }
+
+  cout<< "The given numbers in your array are: "<<endl;
+  cout<< "[ ";
+}
