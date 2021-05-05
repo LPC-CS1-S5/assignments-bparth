5adf9e64ee28e0d3fc9b826df7b4aacbbbc8a1d8
Parth <>
2021-05-05 03:22:40 +0000

    most of the code done for q3

--- Quiz7/q3.cpp
+++ Quiz7/q3.cpp
@@ -5,7 +5,7 @@ int findmax(int numbers[], int, int);
 const int N = 50;
 int main(){
   //declared all the variables
-  int numbers[N], numbers1[20], numbers2[20], max1, max2, num, entry;
+  int numbers[N], numbers1[20], numbers2[20], max1, max2, num, entry, a=0;
 
   //asks how many items does the person want in the array
   cout<< "Enter the number of items you want in your array: ";
@@ -30,6 +30,19 @@ int main(){
       cout<<numbers1[i]<<" , ";
     }else{
       //this stores the other half of the numbers into the second array
+      numbers2[i] = numbers[i];
+      cout<<numbers2[i]<< " , ";
     }
   }
+  cout<< "] "<<endl;
+
+  //function does the max numbers from 1st half of the main array.
+  max1 = findmax(numbers1, a, num/2);
+  cout<<"Max number from first half is: "<< max1<<endl;
+
+  //function does the max numbers from 2nd half of the main array. 
+  max2 = findmax(numbers2, num/2, num);
+  cout<<"Max number from first half is: "<< max2<<endl;
+
+  return 0;
 }
