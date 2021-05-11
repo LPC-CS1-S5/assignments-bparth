d2a33d9f8cc892f0ec3e6868bcdcbd4017d49cef
Parth <>
2021-05-11 04:55:57 +0000

    workng on q1

--- Quiz8/q1.cpp
+++ Quiz8/q1.cpp
@@ -1,4 +1,23 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
+
+int countNum(string &, string&);
+
+int main(){
+
+  
+
+  int txt;
+  int pattern;
+
+}
+
+int countNum(string &pattern,string &txt){
+  ifstream ifs; 
+  ifs.open("python.txt");
+
+  int M = pattern.length();
+}
