c6ee36851bf47e1a613d7baf2800112c40dbaec1
Parth <>
2021-05-27 02:41:27 +0000

    working on the code cant figure out how to compare just yet

--- Final/q3.cpp
+++ Final/q3.cpp
@@ -12,5 +12,15 @@ int numbers1[SIZE1] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
 }
 
 bool isSame(int arr1[], int arr2[], int m, int n){
+int i = 0, j = 0; // establises our two ints that we'll use later on in the code to iterate through the arrays. 
+
+if(m<n){
+  return 0; 
+} //this condition makes sure that if the length of the 1st array is smaller than the second array, then we break the program. 
+
+while(i < n && j<m){
+  
+}
+
 
 }
