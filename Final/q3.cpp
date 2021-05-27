7253bd93a95866c547d3619089ffc2a2ecfced8a
Parth <>
2021-05-27 02:48:12 +0000

    made the function to go through the arrays and see if there is inclusion.

--- Final/q3.cpp
+++ Final/q3.cpp
@@ -19,8 +19,18 @@ if(m<n){
 } //this condition makes sure that if the length of the 1st array is smaller than the second array, then we break the program. 
 
 while(i < n && j<m){
-  
+  if(arr1[j]<arr2[i]){
+    j++;
+    //this if statement checks that our array1's first value matches the second array's first value, and if it doesnt then we increase j by one meaning that now we'll check the second number in the array1 with the fisrt number in array2
+  }else if(arr1[j]==arr2[i]){
+    j++;
+    i++;
+    // this if setement basically acknowledges that both the values at that index are the same so then i and j are increased by one to check the next index. 
+  }else if(arr1[j]>arr2[i]){
+    return 0;
+    //if we reach here that basically means that the code's run through and that we have reached the end of the array.  
+  }
 }
-
+return (i<n) ? false : true; //sets a conditional return of eitehr true/false depending on how the code executes up top. 
 
 }
