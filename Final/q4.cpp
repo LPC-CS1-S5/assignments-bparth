bb11ccd733aa0e545cbb757804ca05c366309af1
Parth <>
2021-05-27 03:58:45 +0000

    Code works neat on VS code but not on replit. going to do the canvas response now

--- Final/q4.cpp
+++ Final/q4.cpp
@@ -7,9 +7,9 @@ void findStr(string d, char t[]);
 int main(){
 char  userinput[20]; // made a character array for the user input instead of a string, since this is easier.
 
-string  words= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quibus natura iure responderit non esse verum aliunde finem beate vivendi, a se principia rei gerendae peti; Quae enim adhuc protulisti, popularia sunt, ego autem a te elegantiora desidero. Duo Reges: constructio interrete. Tum Lucius: Mihi vero ista valde probata sunt, quod item fratri puto. Bestiarum vero nullum iudicium puto. Nihil enim iam habes, quod ad corpus referas; Deinde prima illa, quae in congressu solemus: Quid tu, inquit, huc? Et homini, qui ceteris animantibus plurimum praestat, praecipue a natura nihil datum esse dicemus?";
+string words= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quibus natura iure responderit non esse verum aliunde finem beate vivendi, a se principia rei gerendae peti; Quae enim adhuc protulisti, popularia sunt, ego autem a te elegantiora desidero. Duo Reges: constructio interrete. Tum Lucius: Mihi vero ista valde probata sunt, quod item fratri puto. Bestiarum vero nullum iudicium puto. Nihil enim iam habes, quod ad corpus referas; Deinde prima illa, quae in congressu solemus: Quid tu, inquit, huc? Et homini, qui ceteris animantibus plurimum praestat, praecipue a natura nihil datum esse dicemus?";
 
-cout << "Enter user string "; // gives out the terminal output to the user
+cout << "Enter user string "<<endl; // gives out the terminal output to the user
 cin >> userinput; // sabse the user input into the char array made above.
 
 findStr(words, userinput);
