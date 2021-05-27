a3de75eab5f98c939cfd82697544cb6f18f467b7
Parth <>
2021-05-27 01:31:14 +0000

    made it so that it outputs me the i and the j values and the array,

--- Final/q1.cpp
+++ Final/q1.cpp
@@ -13,11 +13,18 @@ int main() {
         //printout(numbers, N);
 }
 void bubble(int numbers[], int last) {
-        for (int i = 0; i < last - 1; i++) {
-                for (int j = 0; j < last - i - 1; j++)
-                        if (numbers[j] > numbers[j + 1])
-                                swap(numbers[j], numbers[j + 1]);
+  for (int i = 0; i < last - 1; i++) {
+    for (int j = 0; j < last - i - 1; j++){
+      if(numbers[j] > numbers[j + 1]){
+        cout<<"i: "<< i << " j: " <<j << " num[j] = "<< numbers[j] << " num[j+1] = " << numbers[j+1]<<endl; // will qrite out the i and the j values and the array values for me. like a break point. 
+        swap(numbers[j], numbers[j + 1]);
+
+        for(int i = 0; i<last;i++){
+          cout<<numbers[i]<<endl;
         }
+      }
+    }
+  }
 }
 void printout(int numbers[], int last) {
         for (int i = 0; i < last; i++)
