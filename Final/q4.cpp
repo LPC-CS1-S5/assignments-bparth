be269cf5a4e9a9bf8d577d3bc9a4eafed43327c8
Parth <>
2021-05-27 03:24:16 +0000

    woking on the code making sure there's no errors

--- Final/q4.cpp
+++ Final/q4.cpp
@@ -1,5 +1,19 @@
-string  userinput;
+#include <iostream>
+#include <string>
+using namespace std;
+
+void findStr(string d, char t[]);
+
+int main(){
+char  userinput[20]; // made a character array for the user input instead of a string, since this is easier.
+
 string  words= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quibus natura iure responderit non esse verum aliunde finem beate vivendi, a se principia rei gerendae peti; Quae enim adhuc protulisti, popularia sunt, ego autem a te elegantiora desidero. Duo Reges: constructio interrete. Tum Lucius: Mihi vero ista valde probata sunt, quod item fratri puto. Bestiarum vero nullum iudicium puto. Nihil enim iam habes, quod ad corpus referas; Deinde prima illa, quae in congressu solemus: Quid tu, inquit, huc? Et homini, qui ceteris animantibus plurimum praestat, praecipue a natura nihil datum esse dicemus?";
 
 cout << "Enter user string "; 
 cin >> userinput;
+
+}
+
+void findStr(string s, char t[]){
+  
+}
