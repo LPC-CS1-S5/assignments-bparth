3f7fc541011862d73d2dab34387bbba3162438d4
Parth <>
2021-05-27 02:31:09 +0000

    working on the code

--- Final/q3.cpp
+++ Final/q3.cpp
@@ -1,4 +1,16 @@
-const int SIZE1=5;
-const int SIZE2=10;
-int numbers1[SIZE1] = {2, 3, 5, 9, 10};
-int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
+
+#include <iostream>
+using namespace std; 
+bool isSame(int arr1[], int arr2[], int int);
+
+int main(){
+const int SIZE1=10;
+const int SIZE2=5;
+int numbers2[SIZE2] = {2, 3, 5, 9, 10};
+int numbers1[SIZE1] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
+
+}
+
+bool isSame(int arr1[], int arr2[], int m, int n){
+
+}
